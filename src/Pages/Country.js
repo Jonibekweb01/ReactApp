37f7ddd59012fe225ba7ca9c911c7fa68f0cc9e2
Jonibekweb01 <jonibekqodirovv@gmail.com>
2022-11-24 16:12:21 +0500

    Finish

--- src/Pages/Country.js
+++ src/Pages/Country.js
@@ -28,67 +28,65 @@ export const Country = () => {
         <>
             {click.isLoading && (<h1>Loading...</h1>)}
             {click.isError && (<h1>Error</h1>)}
-            {click.data.length && (
-                <div>
-                    <Link className="text-decoration-none text-black h5 pt-5 ps-5 d-block" to="/"> ◀ Back to Home</Link>
-                    {
-                        click.data.map((el) => (
-                            <div className="d-flex align-items-center justify-content-center my-5 gap-5" >
-                                <img src={el.flags.png} alt="" className="w-50" />
-                                <div>
-                                    <h1>{el.name.common}</h1>
-                                    <div className="row">
-                                        <div>
-                                            <p>
-                                                Native name : <span>{el.name.nativeName[Object.keys(el.name.nativeName)[0]].common}</span>
-                                            </p>
-                                            <p>
-                                                Population :
-                                                <span>
-                                                    {el.population}
-                                                </span>
-                                            </p>
-                                            <p>
-                                                Region : <span>{el.region}</span>
-                                            </p>
-                                            <p>
-                                                Sub Region : <span>
-                                                    {el.subregion}
-                                                </span>
-                                            </p>
-                                            <p>
-                                                Capital : <span>
-                                                    {el.capital}
-                                                </span>
-                                            </p>
-                                        </div>
-                                        <div>
-                                            <p>Top Level Domain <span>{el.tld}</span></p>
-                                            <p>Currencies <span>{el.currencies[Object.keys(el.currencies)].name}</span></p>
-                                            <p>Languages  : {''}<span>{el.languages[Object.keys(el.languages)[0]]}</span></p>
-                                        </div>
-                                    </div>
-                                    {
-                                        el?.borders.length && (
+            {click.data.length &&
+                (
+                    <div>
+                        <Link className="text-decoration-none text-black h5 pt-5 ps-5 d-block" to="/"> ◀ Back to Home</Link>
+                        {
+                            click.data.map((el) => (
+                                <div className="d-flex align-items-center justify-content-center my-5 gap-5" >
+                                    <img src={el.flags.png} alt="" className="w-50" />
+                                    <div>
+                                        <h1>{el.name.common}</h1>
+                                        <div className="row">
                                             <div>
-                                                Border Countries:{''}
-                                                {el.borders.map(el => (
-                                                    <button>{el}</button>
-                                                ))}
+                                                <p>
+                                                    Native name : <span>{el.name.nativeName[Object.keys(el.name.nativeName)[0]].common}</span>
+                                                </p>
+                                                <p>
+                                                    Population :
+                                                    <span>
+                                                        {el.population}
+                                                    </span>
+                                                </p>
+                                                <p>
+                                                    Region : <span>{el.region}</span>
+                                                </p>
+                                                <p>
+                                                    Sub Region : <span>
+                                                        {el.subregion}
+                                                    </span>
+                                                </p>
+                                                <p>
+                                                    Capital : <span>
+                                                        {el.capital}
+                                                    </span>
+                                                </p>
                                             </div>
-                                        )
-                                    }
+                                            <div>
+                                                <p>Top Level Domain <span>{el.tld}</span></p>
+                                                <p>Currencies <span>{el.currencies[Object.keys(el.currencies)].name}</span></p>
+                                                <p>Languages  : {''}<span>{el.languages[Object.keys(el.languages)[0]]}</span></p>
+                                            </div>
+                                        </div>
+                                        {
+                                            el?.borders.length && (
+                                                <div>
+                                                    Border Countries:{''}
+                                                    {el.borders.map(el => (
+                                                        <button>{el}</button>
+                                                    ))}
+                                                </div>
+                                            )
+                                        }
+                                    </div>
                                 </div>
-                            </div>
-                            // ||
-                            // <div className="d-flex align-items-center justify-content-center my-5 gap-5" >
-                            //     <img src={el.flags.svg} alt="" className="w-50" />
-                            // </div>
-                        )
-                        )
-                    }
-                </div>
-            )}
+                            )
+                            )
+                        }
+                    </div>
+                )
+            }
         </>
     );
 }
